2807662f21f5b722499f1fe1fe0728f087a2c761
nayla souza ribeiro <naylasouzariibeiro@gmail.com>
2020-05-23 21:25:59 -0300

    Mudando fotos ilustrativa

--- javascript/produtos.js
+++ javascript/produtos.js
@@ -3,35 +3,35 @@ const produtos = [
       nome: 'Pão integral 6 grãos 450g',
       descricao: 'contém ovos',
       valor: 'R$10,00' ,
-      imgUrl: './img/pao-integral-03.jpg'
+      imgUrl: './img/pao-integral-03 _ilustrativo.jpg'
    
    }, 
    { 
       nome: 'Pão Vegano 450g',
       descricao: 'contém glúten',
       valor: 'R$10,00',
-      imgUrl: './img/Pão-Caseiro-Fofinho.png'
+      imgUrl: './img/pao_vegano-ilustrativa.jpg'
 
    }, 
    { 
       nome: 'Bolo de Banana c/ aveia 450g',
       descricao: 'sem glúten, sem adição de açúcar, contém ovos',
       valor: 'R$15,00',
-      imgUrl: './img/bolo-de-banana.jpg'
+      imgUrl: './img/bolo-de-banana-ilustrativo.jpg'
 
    },
    { 
       nome: 'Bolo de Cacau 450g',
       descricao: 'Vegano, contén glúten',
       valor: 'R$15,00',
-      imgUrl: './img/bolo-de-cacau.jpg'
+      imgUrl: './img/bolo-de-cacau-ilustrativo.jpg'
 
    },  
    { 
       nome: 'Bolo de Milho 500g',
       descricao: ' Sem glúten, sem leite, contém ovos ',
       valor: 'R$20,00',
-      imgUrl: './img/bolo-de-milho.png'
+      imgUrl: './img/bolo-de-milho-ilustrativo.jpg'
    },
 
   
@@ -39,35 +39,35 @@ const produtos = [
    nome: 'Mousse de Maracujá 120g',
    descricao: 'contém leite',
    valor: 'R$7,00',
-   imgUrl: './img/mousse-maracuja.png'
+   imgUrl: './img/mousse-maracuja-ilustrativo.jpg'
 },
 
 { 
    nome: 'Geleia de Chia Sabor Uva 230 ml',
    descricao: 'Sem Glúten, produzido com açúcar orgânico ou xilitol (adoçante natural)',
    valor: 'R$15,00',
-   imgUrl: './img/geleia-de-chia2.jpg'
+   imgUrl: './img/geleia-de-chia-ilustrativo.jpg'
 },
 
 {
 nome: 'Creme de Morango 100g',
    descricao: 'Feita a base de inhame e morango, contém açúcar ',
    valor: 'R$6,00',
-   imgUrl: './img/Iogurte_Vegano_Morango.jpg'
+   imgUrl: './img/cremedemorango-ilustrativo.jpg'
 },
 
 {
    nome: 'Cestas Com Caneca Personalizada ',
       descricao: 'contém: Pão Integral ou Vegano, Geleia de Chia, Bolo de 150g, Cupcakes de bolo de banana ',
       valor: ' A partir R$90,00',
-      imgUrl: './img/cesta-de-comidas.jpg'
+      imgUrl: './img/cesta-comcaneca-ilustrativo.jpg'
    },
 
    {
       nome: 'Cestas Sem Caneca Personalizada ',
          descricao: 'contém: Pão Integral ou Vegano, Geleia de Chia, Bolo de 150g, Cupcakes de bolo de banana ',
          valor: ' A partir R$70,00',
-         imgUrl: './img/cesta-com-vinho.jpg'
+         imgUrl: './img/cestasemcaneca-ilustrativo.jpg'
       },
 
       {
